bf6e0eb4f9f48257946fc8bd477aaf74ac09090e
Nikolai <krezi3927@gmail.com>
2024-12-18 00:02:35 +0900

    console

--- middleware/checkRoleModdleware.js
+++ middleware/checkRoleModdleware.js
@@ -10,6 +10,8 @@ module.exports = function(role){
     
         try {
             const token = req.headers.authorization.split(' ')[1] // Bearer
+            console.log(req)
+            console.log(req.headers)
             if(!token){
                 return res.status(401).json({message : "not authorized"})
             }
