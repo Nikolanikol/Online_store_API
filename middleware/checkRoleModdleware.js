d3a92f8bcd51989515253cc972fe0f63ed98b16b
Nikolai <krezi3927@gmail.com>
2024-12-18 00:07:30 +0900

    authmiddlremove

--- middleware/checkRoleModdleware.js
+++ middleware/checkRoleModdleware.js
@@ -3,28 +3,29 @@ const jsonwebtoken = require('jsonwebtoken')
 
 module.exports = function(role){
     return function(req,res, next){
-        if(res.method=== "OPTIONS"){
-            next()
-        }
+        next()
+        // if(res.method=== "OPTIONS"){
+        //     next()
+        // }
     
     
-        try {
-            const token = req.headers.authorization.split(' ')[1] // Bearer
-            console.log(req)
-            console.log(req.headers)
-            if(!token){
-                return res.status(401).json({message : "not authorized"})
-            }
-            const decoded = jsonwebtoken.verify(token, process.env.SECRET_KEY)
-            if(decoded.role !== role){
-                return res.status(403).json({message : "no access"})
-            }
+        // try {
+        //     const token = req.headers.authorization.split(' ')[1] // Bearer
+        //     console.log(req)
+        //     console.log(req.headers)
+        //     if(!token){
+        //         return res.status(401).json({message : "not authorized"})
+        //     }
+        //     const decoded = jsonwebtoken.verify(token, process.env.SECRET_KEY)
+        //     if(decoded.role !== role){
+        //         return res.status(403).json({message : "no access"})
+        //     }
 
-            req.user = decoded
-            next()
-        } catch (error) {
-            res.status(401).json({message : "not authorized"})
-        }
+        //     req.user = decoded
+        //     next()
+        // } catch (error) {
+        //     res.status(401).json({message : "not authorized"})
+        // }
     }
 
 
